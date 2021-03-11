b89f49bab1bba44c6c85d9033850d1cd67fa98e1
Vibhuti53 <79911151+Vibhuti53@users.noreply.github.com>
2021-03-11 16:56:38 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,7 +10,10 @@ function draw() {
   background(255,255,255);  
   movingRect.x=World.mouseX;
   movingRect.y=World.mouseY;
-if(movingRect.x-fixedRect.x<fixedRect.width/2+movingRect.width/2  && fixedRect.x-movingRect.x<fixedRect.width/2+movingRect.width/2 && movingRect.y-fixedRect.y<fixedRect.height/2+movingRect.height/2 && fixedRect.y-movingRect.y<fixedRect.height/2+movingRect.height/2){
+if(movingRect.x-fixedRect.x<fixedRect.width/2+movingRect.width/2 && 
+   fixedRect.x-movingRect.x<fixedRect.width/2+movingRect.width/2 && 
+   movingRect.y-fixedRect.y<fixedRect.height/2+movingRect.height/2 && 
+   fixedRect.y-movingRect.y<fixedRect.height/2+movingRect.height/2){
   movingRect.shapeColor="red";
   fixedRect.shapeColor="red";
 }
